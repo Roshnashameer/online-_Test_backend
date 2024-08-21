a4d26551a5ee694d4524413e271bbcf98085e6fb
roshnashameer <roshnasharaf123@gmail.com>
2024-08-21 11:25:19 +0530

    sample

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -8,7 +8,7 @@ exports.jwtMiddleware = (req, res, next) => {
             throw new Error("Authorization header missing");
         }
         const token = header.split(" ")[1];
-        // console.log(token);
+        console.log(token);
         const JWTresponse = jwt.verify(token, 'supersecretkey123');
         
         req.payload = JWTresponse._id;
